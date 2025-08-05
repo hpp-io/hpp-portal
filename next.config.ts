dc3c9cb4c5aec571ad2783b44c7bdbf9d45b67d6
Hun Im <dev_hun@aergo.io>
2025-08-05 18:20:03 +0900

    Add HPP Mainnet logo SVG

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
