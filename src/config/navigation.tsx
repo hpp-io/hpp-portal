1ae2e132ef57140876bef8813e895bc8c936791b
Hun Im <dev_hun@aergo.io>
2025-08-06 17:43:34 +0900

    Update bridge navigation order

--- src/config/navigation.tsx
+++ src/config/navigation.tsx
@@ -26,6 +26,16 @@ export const navItems = [
     ),
   },
   {
+    label: 'Bridge',
+    href: 'https://bridge.arbitrum.io/?sourceChain=ethereum&destinationChain=hpp-mainnet&tab=bridge',
+    external: true,
+    icon: (
+      <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+      </svg>
+    ),
+  },
+  {
     label: 'Airdrop',
     icon: (
       <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -35,16 +45,6 @@ export const navItems = [
           strokeWidth={2}
           d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
         />
-      </svg>
-    ),
-  },
-  {
-    label: 'Bridge',
-    href: 'https://bridge.arbitrum.io/?sourceChain=ethereum&destinationChain=hpp-mainnet&tab=bridge',
-    external: true,
-    icon: (
-      <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
       </svg>
     ),
   },
