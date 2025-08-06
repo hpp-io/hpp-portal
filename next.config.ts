fb897c7356cf754e13974911a98f82f7d2ca22e8
Instant Coffee <73890790+findmytrueself@users.noreply.github.com>
2025-08-06 15:10:22 +0900

    Revert "Remove @reown/appkit connector and add custom connector modal (#6)" (#9)
    This reverts commit 2cc948ce82363971a682fa6fc8e766d4bef91110.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ...(process.env.NODE_ENV === 'production' && { output: 'export' }),
+  output: 'export',
   images: {
     remotePatterns: [
       {
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,6 +1,6 @@
-import { cookieStorage, createStorage, http, createConfig } from '@wagmi/core';
-import { mainnet, arbitrum } from 'wagmi/chains';
-import { metaMask, walletConnect, coinbaseWallet } from 'wagmi/connectors';
+import { cookieStorage, createStorage, http } from '@wagmi/core';
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+import { mainnet, arbitrum } from '@reown/appkit/networks';
 
 // Get projectId from https://dashboard.reown.com
 export const projectId = process.env.NEXT_PUBLIC_APP_KIT_PROJECT_ID;
@@ -11,28 +11,14 @@ if (!projectId) {
 
 export const networks = [mainnet, arbitrum];
 
-// Set up the Wagmi Config
-export const config = createConfig({
-  chains: [mainnet, arbitrum],
+//Set up the Wagmi Adapter (Config)
+export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
     storage: cookieStorage,
   }),
   ssr: true,
-  connectors: [
-    metaMask(),
-    walletConnect({ projectId }),
-    coinbaseWallet({
-      appName: 'HPP Portal',
-      // Use version 4 for the latest Coinbase Wallet SDK
-      version: '4',
-      // Set preference to 'all' to show all wallet options
-      preference: 'all',
-      // Set app logo URL if available
-      appLogoUrl: process.env.NEXT_PUBLIC_APP_LOGO_URL,
-    }),
-  ],
-  transports: {
-    [mainnet.id]: http(),
-    [arbitrum.id]: http(),
-  },
+  projectId,
+  networks,
 });
+
+export const config = wagmiAdapter.wagmiConfig;
