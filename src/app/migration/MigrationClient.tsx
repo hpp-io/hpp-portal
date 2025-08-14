a7caecd9bb899373a893002e9e94eb587b571c46
Hun Im <dev_hun@aergo.io>
2025-08-14 16:04:51 +0900

    Update Arb Bridge URL query

--- src/app/migration/MigrationClient.tsx
+++ src/app/migration/MigrationClient.tsx
@@ -1314,7 +1314,7 @@ export default function MigrationClient() {
                         <Button
                           variant="primary"
                           size="lg"
-                          href="https://bridge.arbitrum.io/?sourceChain=ethereum&destinationChain=hpp-mainnet&tab=bridge"
+                          href="https://bridge.arbitrum.io/?destinationChain=190415&sourceChain=ethereum&token=0xe33fbe7584eb79e2673abe576b7ac8c0de62565c"
                           external={true}
                           className="flex items-center justify-center space-x-2 mx-auto"
                         >
