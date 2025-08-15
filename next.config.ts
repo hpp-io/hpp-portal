8bcebf59b1c6532205c112acac52b2cfc51f7eb5
Hun Im <dev_hun@aergo.io>
2025-08-15 21:18:13 +0900

    Remove the experimental option

--- next.config.ts
+++ next.config.ts
@@ -14,13 +14,11 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
   },
