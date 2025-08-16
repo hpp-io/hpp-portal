e5adb6a4b240f9d872fc337b97d7111dca772a64
Hun Im <dev_hun@aergo.io>
2025-08-17 00:21:29 +0900

    Change logo and menu positions, update logo click to route to Home

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import { MobileMenuIcon, BackIcon, HPPPortalIcon } from '@/assets/icons';
 // import WalletButton from '@/components/ui/WalletButton';
 
@@ -17,6 +18,12 @@ export default function Header({ onMenuClick, isSidebarOpen = false, onBackClick
       <div className="max-[1200px]:block hidden bg-black border-b border-[#161616] px-4 h-[66px]">
         <div className="grid grid-cols-3 items-center h-full">
           <div className="flex">
+            <Link href="/" aria-label="Go to home" className="cursor-pointer">
+              <HPPPortalIcon className="w-auto h-10" />
+            </Link>
+          </div>
+          <div></div>
+          <div className="flex justify-end">
             <button
               onClick={isSidebarOpen ? onBackClick : onMenuClick}
               className="cursor-pointer"
@@ -29,33 +36,15 @@ export default function Header({ onMenuClick, isSidebarOpen = false, onBackClick
               )}
             </button>
           </div>
-          <div></div>
-          <div className="flex justify-end">
-            <a
-              href="https://www.hpp.io"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Open HPP official website"
-              className="cursor-pointer"
-            >
-              <HPPPortalIcon className="w-auto h-10" />
-            </a>
-          </div>
         </div>
       </div>
 
       {/* ≥1200px */}
       <div className="min-[1200px]:block hidden bg-black border-b border-[#161616] px-6 h-[85px]">
         <div className="flex items-center justify-between h-full">
-          <a
-            href="https://www.hpp.io"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Open HPP official website"
-            className="cursor-pointer"
-          >
+          <Link href="/" aria-label="Go to home" className="cursor-pointer">
             <HPPPortalIcon className="w-auto h-10" />
-          </a>
+          </Link>
           {/* <div className="flex items-center">
             <WalletButton />
           </div> */}
