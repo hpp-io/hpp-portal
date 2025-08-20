b7c1e20f7e24a632f616488e6b60aa75f125acc5
Hun Im <dev_hun@aergo.io>
2025-08-20 12:49:15 +0900

    Change button position

--- src/components/ui/NeedHelp.tsx
+++ src/components/ui/NeedHelp.tsx
@@ -9,6 +9,9 @@ export default function NeedHelp() {
         If you're unsure about which migration path to use or need assistance, our support team is here to help.
       </p>
       <div className="flex flex-row gap-4 justify-center items-center">
+      <Button variant="primary" size="lg" className="cursor-pointer w-40 h-12 px-6 py-3">
+          Contact Support
+        </Button>
         <Button
           variant="primary"
           size="lg"
@@ -17,9 +20,6 @@ export default function NeedHelp() {
           className="cursor-pointer w-40 h-12 px-6 py-3"
         >
           FAQ
-        </Button>
-        <Button variant="primary" size="lg" className="cursor-pointer w-40 h-12 px-6 py-3">
-          Contact Support
         </Button>
       </div>
     </div>
